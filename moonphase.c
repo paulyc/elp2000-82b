2dc5d1275ab9c08a65dd942e1e247699a01e60ae
Paul Ciarlo <paul.ciarlo@gmail.com>
2019-10-24 17:25:16 -0400

    fix some math

--- moonphase.c
+++ moonphase.c
@@ -22,15 +22,15 @@ double moonphase(time_t unixt, int use_sidereal_time)
     const double lunar_longitude_degrees = fmod(moon_position.longitude / 3600.0, 360.0); // arcseconds to degrees mod 360
     //const double solar_longitude = solar_longitude_of_date(jd2000_unixt);
 
-    const double jdtime = jd2000_epoch_jd + (unixt - jd2000_epoch_unixtime) / 86400.0;
+    const double jdtime = jd2000_epoch_jd + (unixt - jd2000_epoch_unixtime) / 86400.0 - 0.5;
     const double jd = round(jdtime);
-    const double jdhours = jd - jdtime;
+    const double jdhours = jdtime - jd;
     const double jde = jd + 67.6439/86400.0;
     double solar_longitude0 = get_apparent_sidereal_time(jd, jde);
     const double solar_longitude_interp = jdhours < 0 ? get_apparent_sidereal_time(jd - 1.0, jde - 1.0) : get_apparent_sidereal_time(jd + 1.0, jde + 1.0);
     // interpolate to the hour
     const double solar_longitude_diff = fabs(solar_longitude_interp - solar_longitude0);
-    solar_longitude0 += -jdhours * solar_longitude_diff;
+    solar_longitude0 += jdhours * solar_longitude_diff;
 
     const double solar_longitude1 = solar_longitude_of_date(jd2000_unixt);
 
@@ -51,7 +51,7 @@ static void test_moonphase() {
     time_t t = time(NULL);
     double lastphase = moonphase(t, 1);
     // can't be more than 32 days away so it must be a bug, don't run forever
-    for (time_t giveup = t+32*86400 ; t < giveup; t += 3600) {
+    for (time_t giveup = t+32*86400 ; t < giveup; t += 600) {
         double phase = moonphase(t, 1);
         printf("moonphase %f\n", phase);
         if (fabs(phase - lastphase) > 0.1) {
@@ -75,8 +75,9 @@ static void moonphase_main(int argc, char *argv[]) {
     test_moonphase();
     const double phase = moonphase(time(NULL), 1);
     const double mean_synodic_month_in_solar_days = 29.530588;
+    // really is just solar days since last new moon but good enough estimate
     const double phase_in_mean_solar_days = phase * mean_synodic_month_in_solar_days;
-    printf("Your phase now is %f (%f in mean solar days)\n", phase, phase_in_mean_solar_days);
+    printf("Your phase now is %f (~%f in mean solar days)\n", phase, phase_in_mean_solar_days);
 }
 
 int main(int argc, char *argv[]) {
